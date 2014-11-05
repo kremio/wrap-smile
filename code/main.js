d518de5bc8ae5bf1be4a68c01f5d51445ef4cac2
kremio <kremio.software@gmail.com>
2014-11-05 12:19:55 +0200

    Promisify backend code

--- code/main.js
+++ code/main.js
@@ -1,138 +1,71 @@
-define(['module', 'smiledetection', 'fs', 'exif'], function(module, smile, fs, exif){
-   /*var SmileWrap = {
-    go: function(req, res){
-      try{
-        res.render( "/index", {wrapBaseURL: wrapBaseURL } );
-      }catch(e){
-        console.log(e);
-        res.send(404, e);
-      }
-    },
+define(['module', 'session', 'smiledetection', 'fs', 'exif', 'bluebird'], function(module, Session, smile, fs, exif, Promise){
 
-    verify: function(req, res){
-      var imgType = req.body.img.match(/^data:image\/(png|jpeg);base64,/)[1];
-      var base64Data = req.body.img.replace(/^data:image\/(png|jpeg);base64,/,"");
-      var tmpFileName = __tmpFolder + "/" + (new Date()).getTime()+"."+imgType;
+  Promise.promisifyAll( fs );
+  var exifAsync = Promise.promisify( exif.ExifImage );
 
-      console.log("Saving image file", tmpFileName);
-      
-      fs.writeFile( tmpFileName, base64Data, 'base64', function(err) {
-        if(err){	  
-          console.log(err);
-          res(500,'Could not save image:'+err.message);
-          return;
-        }
-
-        //check for EXIF metadata
-        if(imgType === 'jpeg'){ 
-          var ExifImage = exif.ExifImage;
-          var jpegBuffer = new Buffer(base64Data, 'base64');
-          try {
-            new ExifImage({ image : jpegBuffer }, function (error, exifData) {
-              if (error){
-                console.log('Error: '+error.message);
-                res(500,'Error reading metadata:'+err);
-              }else{
-                //check if the orientation is provided
-                if( exifData.image && exifData.image.Orientation && exifData.image.Orientation != 1 ){
-                  //need to rotate image before smile detection
-                  var rotateBy = 0;
-                  switch( exifData.image.Orientation ){
-                    case 3: rotateBy = smile.ROT_180; break;
-                    case 6: rotateBy = smile.ROT_90_CW; break;
-                    case 8: rotateBy = smile.ROT_90_CCW; break;
-                  }
-                  //Check for smiles
-                  smile.find( tmpFileName, __assets+"/cascades/smiled_04.xml", function(isSmiling){
-                    res.send(200, isSmiling);
-                  }, rotateBy);
-                }
-              }
-            });
-          } catch (error) {
-            console.log('Error: ' + error.message);
-            res(500,'Error reading metadata:'+err);
-          }
-        }else{	
-          //Check for smiles
-          smile.find( tmpFileName, __assets+"/cascades/smiled_04.xml", function(isSmiling){
-            res.send(200, isSmiling);
-          });
-        } 
-		  });
-
-    }
-  };
-*/
   var wrapBaseURL =  module.config().wrapBaseURL;
 
 
   var routes = function(app, __dirname,  __assets, __tmpFolder){
-    app.get(wrapBaseURL+'/route/go', function(req, res){
+  
+    app.get(wrapBaseURL+'/', function(req, res){
       try{
-       res.render( "./main", {wrapBaseURL: wrapBaseURL } );
+       res.render( "main", {wrapBaseURL: wrapBaseURL, wrapData: Session.getWrapData(req) } );
       }catch(e){
         console.log(e);
         res.send(404, e);
       }
     });
 
-    app.all(wrapBaseURL+'/route/verify', function(req, res){
+    app.all(wrapBaseURL+'/verify', function(req, res){
       console.log("Smile wrap verify");
 
       var imgType = req.body.img.match(/^data:image\/(png|jpeg);base64,/)[1];
       var base64Data = req.body.img.replace(/^data:image\/(png|jpeg);base64,/,"");
       var tmpFileName = __tmpFolder + "/" + (new Date()).getTime()+"."+imgType;
 
-      console.log("Saving image file", tmpFileName);
-      
-      fs.writeFile( tmpFileName, base64Data, 'base64', function(err) {
-        if(err){	  
-          console.log(err);
-          res(500,'Could not save image:'+err.message);
-          return;
-        }
 
-        //check for EXIF metadata
+      console.log("Saving image file", tmpFileName); 
+      fs.writeFileAsync( tmpFileName, base64Data, 'base64').then( function() {
         if(imgType === 'jpeg'){ 
-          var ExifImage = exif.ExifImage;
+          //check for EXIF metadata
           var jpegBuffer = new Buffer(base64Data, 'base64');
-          try {
-            new ExifImage({ image : jpegBuffer }, function (error, exifData) {
-              if (error){
-                console.log('Error: '+error.message);
-                res(500,'Error reading metadata:'+err);
-              }else{
-                //check if the orientation is provided
-                if( exifData.image && exifData.image.Orientation && exifData.image.Orientation != 1 ){
-                  //need to rotate image before smile detection
-                  var rotateBy = 0;
-                  switch( exifData.image.Orientation ){
-                    case 3: rotateBy = smile.ROT_180; break;
-                    case 6: rotateBy = smile.ROT_90_CW; break;
-                    case 8: rotateBy = smile.ROT_90_CCW; break;
-                  }
-                  //Check for smiles
-                  smile.find( tmpFileName, __assets+"/cascades/smiled_04.xml", function(isSmiling){
-                    res.send(200, isSmiling);
-                  }, rotateBy);
-                }
+          return exifAsync({ image : jpegBuffer }).then( function (exifData) {
+            var rotateBy = false;
+            //check if the orientation is provided
+            if( exifData.image && exifData.image.Orientation && exifData.image.Orientation != 1 ){
+              //need to rotate image before smile detection
+              switch( exifData.image.Orientation ){
+                case 3: rotateBy = smile.ROT_180; break;
+                case 6: rotateBy = smile.ROT_90_CW; break;
+                case 8: rotateBy = smile.ROT_90_CCW; break;
               }
-            });
-          } catch (error) {
-            console.log('Error: ' + error.message);
-            res(500,'Error reading metadata:'+err);
-          }
-        }else{	
-          //Check for smiles
-          smile.find( tmpFileName, __assets+"/cascades/smiled_04.xml", function(isSmiling){
-            res.send(200, isSmiling);
+            }
+            return rotateBy;
           });
-        } 
-		  });
+        }else{
+          return false; //don't perform any rotation prior to the detection
+        }
+      }).then( function( rotateBy){ //Smile detection
+          return smile.find( tmpFileName, __assets+"/cascades/smiled_04.xml", rotateBy );
+      }).then( function(isSmiling){ //Return result back to client
+        if(isSmiling){ //Mark the wrap as completed and save any extra info
+          var wrapData = Session.getWrapData(req);
+          wrapData.completed = true;
+          wrapData.extra = { image: tmpFileName };
+          Session.setWrapData(req, wrapData);
+        }
+        res.send(200, isSmiling);
+      }).catch( function(err){
+        console.log(err);
+        res(500,'Could not analyze image:'+err.message);
+        return;
+      });
+     
+   });
 
-    });
-  };
+  };//End of HTTP handlers
+
 
 
   return routes;
--- code/smiledetection.js
+++ code/smiledetection.js
@@ -1,66 +1,68 @@
-define(["opencv"], function( cv ){
+define(['opencv', 'bluebird'], function( cv, Promise ){
 
-	var smile = {
-		//in degrees of angle, positive values mean counter clockwise rotation
-		ROT_180: 180,
-                ROT_90_CW: -90,
-                ROT_90_CCW: 90
-	};
+  var cv_readImageAsync = Promise.promisify( cv.readImage );
+  Promise.promisifyAll( cv.Matrix.prototype, {suffix: "MyAsync"} ); //the 'Async' prefix is already used by opencv
 
-	smile.find = function(inputImgPath, smileCascadeFile, callback, rotateBy){
-		cv.SMILE_CASCADE = smileCascadeFile;
-		//cv.SMILE_CASCADE = __dirname+"/cascades/haarcascade_smile.xml";
-		//cv.SMILE_CASCADE = __dirname+"/cascades/smiled_04.xml"; //https://github.com/hromi/SMILEsmileD
-		
-		var facesFound;
-		var imageLoaded;
+  var smile = {
+    //in degrees of angle, positive values mean counter clockwise rotation
+    ROT_180: 180,
+ROT_90_CW: -90,
+ROT_90_CCW: 90
+  };
 
-		var processNextFace = function(smileFound){
-			if(smileFound){
-				callback( smileFound );
-				return;
-			}
-			if( facesFound.length < 1 ){
-				callback( false );
-				return;
-			}
-			var face = facesFound.pop();
-                        var faceImg = imageLoaded.roi(face.x, face.y, face.width, face.height);
-                        faceImg.detectObject(cv.SMILE_CASCADE, {}, function(err, smiles){
-			    if( smiles && smiles.length > 0 ){
-				console.log(smiles.length + " smile(s) found");
-				var biggestSmile = 0;
-				for(var s = 1; s < smiles.length; s++){
-					if( smiles[s].width * smiles[s].height > smiles[biggestSmile].width *  smiles[biggestSmile].height ){
-						biggestSmile = s;
-					}
-				}
-				//add the face coordinates to the smile coordinates
-				smiles[biggestSmile].x += face.x;
-				smiles[biggestSmile].y += face.y;
-				processNextFace( smiles[biggestSmile] );
-			    }else{
-				processNextFace( false );
-			    }
-                        });
+  smile.find = function(inputImgPath, smileCascadeFile, rotateBy){
+    cv.SMILE_CASCADE = smileCascadeFile;
 
-		};
+    var facesFound, imageLoaded;
 
-		cv.readImage( inputImgPath, function(err, im){
-		  if(rotateBy){
-			im.rotate(rotateBy);
-		  }
-		  imageLoaded = im;
+    //Look for a smile in the next found face
+    var processNextFace = function(smileFound){
+      if(smileFound){
+        return smileFound;
+      }
+      if( facesFound.length < 1 ){
+        return false;
+      }
 
-		  im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
-		    console.log("Found "+faces.length+" faces.");
-		    facesFound = faces;
- 		    processNextFace(false);
-		  });
+      var face = facesFound.pop();
+      var faceImg = imageLoaded.roi(face.x, face.y, face.width, face.height);
 
-		});
+      //Try and find a smile on the detected face
+      return faceImg.detectObjectMyAsync(cv.SMILE_CASCADE, {}).then( function(smiles){
+        if( smiles && smiles.length > 0 ){
+          var biggestSmile = 0;
+          for(var s = 1; s < smiles.length; s++){
+            if( smiles[s].width * smiles[s].height > smiles[biggestSmile].width *  smiles[biggestSmile].height ){
+              biggestSmile = s;
+            }
+          }
+          //add the face coordinates to the smile coordinates
+          smiles[biggestSmile].x += face.x;
+          smiles[biggestSmile].y += face.y;
+          return processNextFace( smiles[biggestSmile] );
+        }
 
-	}
+        return processNextFace( false );
+      });
+
+    };
+
+    //Process the image with OpenCV to detect faces
+    return cv_readImageAsync( inputImgPath ).then( function(im){
+      if(rotateBy){
+        im.rotate(rotateBy);
+      }
+      return im;
+    }).then( function(im){
+      imageLoaded = im; //keep a reference to the image so we can extract part of it later
+      return im.detectObjectMyAsync(cv.FACE_CASCADE, {});
+    }).then( function(faces){
+      facesFound = faces; //An array of positions and dimensions of detected faces on the image
+      return processNextFace(false); //Try and find smiles on the detected faces
+    });
+
+
+  };
 
 	return smile;
 
